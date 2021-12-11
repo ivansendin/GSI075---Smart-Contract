011c96e77a3c9737896c1097c2591c1c9b1b299a
Ivan Sendin <ivansendin@yahoo.com>
2021-12-11 17:04:32 -0300

    Create balance.py

---
+++ Assignments/Week2/balance.py
@@ -0,0 +1,20 @@
+# This week you should import your addres to MetaMask and get some
+# rETHER using one faucet
+# I will use this script (with correct a list) to check
+
+a = [
+    ['Name',	'0xB51SOMEADDRES'],
+    ['Other',	'0xBCCSOMEADDRES']
+ ]
+
+
+
+
+from web3 import Web3
+INFURASTR='https://ropsten.infura.io/v3/YOURINFURASTR'
+w3 = Web3(Web3.HTTPProvider(INFURASTR))
+
+for _a in a:
+    b = w3.eth.getBalance(_a[1])
+    if b>0:
+        print(_a,b)
